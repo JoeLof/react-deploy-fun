3e7f4dbd8b8879b7f64ec1531c1c9208043bc999
JoeLof <joelofranco@hotmail.com>
2022-11-28 12:19:37 -0500

    aaron deploy

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello This is the PRODUCTION branch</h1>
+      <h2>Aaron tried this with a react app before and it was sooooooooooooooo complicated!!</h2>
+      <h3>Now this is soooooooooo easy!</h3>
     </div>
   );
 }
